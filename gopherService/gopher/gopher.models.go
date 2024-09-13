7f42cc354f547072d5127391344c65361c26352b
Ashton Holgate <ashton.holgate@gmail.com>
2024-09-13 16:00:11 +0100

    feat: add depth to gopher

--- gopherService/gopher/gopher.models.go
+++ gopherService/gopher/gopher.models.go
@@ -5,10 +5,16 @@ import (
 	"strings"
 )
 
+type Address struct {
+	Street   string
+	Postcode string
+}
+
 type BaseGopher struct {
-	Name  string `json:"name"`
-	Age   *int   `json:"age"`
-	Color string `json:"color"`
+	Name    string   `json:"name"`
+	Age     *int     `json:"age"`
+	Color   string   `json:"color"`
+	Address *Address `json:"address"`
 }
 
 type IncomingGopher struct {
@@ -21,7 +27,7 @@ type OutgoingGopher struct {
 }
 
 func (g *IncomingGopher) Validate() error {
-	issues := []string{}
+	var issues []string
 
 	if g.Name == "" {
 		issues = append(issues, "name is required")
@@ -39,6 +45,18 @@ func (g *IncomingGopher) Validate() error {
 	if strings.ToLower(g.Color) == "red" {
 		issues = append(issues, "color can not be red")
 	}
+
+	if g.Address == nil {
+		issues = append(issues, "address is required")
+	} else {
+		if g.Address.Street == "" {
+			issues = append(issues, "address.street is required")
+		}
+		if g.Address.Postcode == "" {
+			issues = append(issues, "address.postcode is required")
+		}
+	}
+
 	if len(issues) > 0 {
 		return &customErrors.ValidationError{Issues: strings.Join(issues, ", ")}
 	}
